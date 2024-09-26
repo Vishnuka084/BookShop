e448bff66e9ee4c4af07712e8b51d05c4c3ae0ff
vishnuk084 <vishnukayahan84@gmail.com>
2024-09-27 02:28:37 +0530

    Updated project and testing the data

--- doctor-service/src/main/java/com/levelUp/doctor_service/entity/Doctor.java
+++ doctor-service/src/main/java/com/levelUp/doctor_service/entity/Doctor.java
@@ -1,21 +1,24 @@
 package com.levelUp.doctor_service.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "doctor")
 public class Doctor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "specialization")
     private String specialization;
+
+    @Column(name = "available_time_slots")
     private String availableTimeSlots;
 
 }
