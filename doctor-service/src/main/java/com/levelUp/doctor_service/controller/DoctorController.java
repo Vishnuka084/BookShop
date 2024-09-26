c5e6f56a9fb214200cf67fb5f2aa3641bd5628fe
vishnuk084 <vishnukayahan84@gmail.com>
2024-09-27 01:36:51 +0530

    Updated folder struceet

---
+++ doctor-service/src/main/java/com/levelUp/doctor_service/controller/DoctorController.java
@@ -0,0 +1,36 @@
+package com.levelUp.doctor_service.controller;
+
+import com.levelUp.doctor_service.entity.Doctor;
+import com.levelUp.doctor_service.service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/doctors")
+public class DoctorController {
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @GetMapping
+    public List<Doctor> getAllDoctors() {
+        return doctorService.getAllDoctors();
+    }
+
+    @GetMapping("/{id}")
+    public Doctor getDoctorById(@PathVariable Long id) {
+        return doctorService.getDoctorById(id);
+    }
+
+    @PostMapping
+    public Doctor createDoctor(@RequestBody Doctor doctor) {
+        return doctorService.createDoctor(doctor);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDoctor(@PathVariable Long id) {
+        doctorService.deleteDoctor(id);
+    }
+}
